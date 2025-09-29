18bf217fd9e8da4251d3e7ae225e6f007414faeb
alex7090 <adecharry@gmail.com>
2025-09-29 22:50:06 +0200

    <3

--- front/src/components/GuessForm.js
+++ front/src/components/GuessForm.js
@@ -271,7 +271,7 @@ const GuessForm = ({ onSuccess, submitted }) => {
                   required
                   min="1500"
                   max="7000"
-                  step="10"
+                  step="1"
                   className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent"
                   placeholder="3500"
                 />
